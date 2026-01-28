755b41cf14772364469c4ea2e133f4a81cc699cc
Enzo-Malvaud <enzo.malvaud@gmail.com>
2026-01-28 16:36:06 +0100

    sécurisation des champs signin signup en js, verification chaine charactère + adaptation zone avis page acueil pour le responsive

--- frontend/js/auth/signin.js
+++ frontend/js/auth/signin.js
@@ -1,9 +1,36 @@
+
+const signinForm = document.getElementById("signinForm");
+let champSignin = getChampSignin();
+signinForm.innerHTML = champSignin;
 const inputMail = document.getElementById("EmailInput");
 const inputPassword = document.getElementById("PasswordInput");
 const btnSignin = document.getElementById("btnSignin");
-const signinForm = document.getElementById("signinForm");
 
 btnSignin.addEventListener("click", checkCredentials );
+
+
+function getChampSignin(){
+   let email = sanitizeHtml();
+   let mdp = sanitizeHtml();
+   return `<div class="mb-3">
+      <p>pour profiter de tous nos services connectez-vous</p>
+      <label for="EmailInput" class="form-label">Email</label>
+      <input type="email" class="form-control" id="EmailInput" placeholder="test@mail.fr" value="${email}" name="email">
+    </div>
+    <div class="mb-3">
+      <label for="PasswordInput" class="form-label">Mot de passe</label>
+      <input type="password" class="form-control" id="PasswordInput" value="${mdp}" name="mdp">
+            <div class="invalid-feedback">
+        Le mail et, ou le mot de passe ne correspondent pas.
+      </div>
+    </div >    
+    <div class="text-center">
+    <button type="button" class="btn btn-primary" id="btnSignin">connectez-vous</button>
+    </div> `;
+
+}
+
+
 
 
 function checkCredentials() {
