b7a1071b2dc4abfffba02acc145c1cdc203c47cc
Enzo-Malvaud <enzo.malvaud@gmail.com>
2026-02-25 17:35:51 +0100

    corrections page principal layout créations début pages vue ensembles menus

--- frontend/Router/allRoutes.js
+++ frontend/Router/allRoutes.js
@@ -3,8 +3,10 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "pages/home.html", []),
+    new Route("/ourmenus", "Ourmenus", "/pages/ourmenus.html", []),
     new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "/js/auth/signin.js"),
     new Route("/signup", "inscription", "/pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"),
+    
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
