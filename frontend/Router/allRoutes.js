a67931e9904e3a230119cd17a66f938d7a084c32
Enzo-Malvaud <enzo.malvaud@gmail.com>
2026-03-10 10:16:27 +0100

    modifications page home , style sass, page 404, ajout route autre pages divers

--- frontend/Router/allRoutes.js
+++ frontend/Router/allRoutes.js
@@ -3,11 +3,16 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "pages/home.html", []),
-    new Route("/ourmenus", "Ourmenus", "/pages/ourmenus.html", []),
+    new Route("/ourmenus", "Nos menus", "/pages/menu/ourmenus.html", []),
+    new Route("/detailmenu", "Detail Menu", "/pages/menu/detailmenu.html", []),
+    new Route("/ourrentals", "Nos locations", "/pages/rental/ourrentals.html", []),
+    new Route("/detailrental", "Detail location", "/pages/rental/detailrental.html", []),
+    new Route("/ordermenu", "Order Menu", "/pages/order/ordermenu.html", ["connected"]),
+    new Route("/contact", "Contact", "/pages/contact.html", []),
+    new Route("/mentionslegales", "Mentions Légales", "/pages/mentionslegales.html", []),
     new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "/js/auth/signin.js"),
-    new Route("/signup", "inscription", "/pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"),
-    
+    new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
-export const websiteName = "Vite et Gourmand";
+export const websiteName = "Vite et Gourmand"; 
