e1732f3389c89c54fd3566d1efc53086c1c02c74
Enzo-Malvaud <enzo.malvaud@gmail.com>
2026-03-31 09:31:34 +0200

    refactor: modications ordre des routes allRoutes.js + plus créations routes pour les roles

--- frontend/Router/allRoutes.js
+++ frontend/Router/allRoutes.js
@@ -2,11 +2,16 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
+    // Routes publiques
     new Route("/", "Accueil", "pages/home.html", []),
     new Route("/ourmenus", "Nos menus", "/pages/menu/ourmenus.html", []),
     new Route("/detailmenu", "Detail Menu", "/pages/menu/detailmenu.html", []),
     new Route("/ourrentals", "Nos locations", "/pages/rental/ourrentals.html", []),
     new Route("/detailrental", "Detail location", "/pages/rental/detailrental.html", []),
+    new Route("/contact", "Contact", "/pages/contact.html", []),
+    new Route("/mentionslegales", "Mentions Légales", "/pages/mentionslegales.html", []),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "/js/auth/signin.js"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"),
     
     // Routes utilisateurs connectés
     new Route("/ordermenu", "Order Menu", "/pages/order/ordermenu.html", ["ROLE_USER"]),
@@ -17,16 +22,12 @@ export const allRoutes = [
     new Route("/employee/orders", "Commandes", "/pages/employee/orders.html", ["ROLE_EMPLOYEE"]),
     
     // Routes administrateurs
-    new Route("/admin/dashboard", "Admin", "/pages/admin/dashboard.html", ["ROLE_ADMIN"]),
+    new Route("/admin/dashboard/stats", "Admin", "/pages/admin/stats.html", ["ROLE_ADMIN"]),
     new Route("/admin/users", "Gérer utilisateurs", "/pages/admin/users.html", ["ROLE_ADMIN"]),
     
-    // Routes publiques
-    new Route("/contact", "Contact", "/pages/contact.html", []),
-    new Route("/mentionslegales", "Mentions Légales", "/pages/mentionslegales.html", []),
-    new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "/js/auth/signin.js"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"),
+    
+    
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Vite et Gourmand"; 
-
